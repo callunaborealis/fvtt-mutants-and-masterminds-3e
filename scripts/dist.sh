b02c5f2a6c5b5d7fa01ce4f2f5d382ebf120cc75
callunaborealis <callunaborealis@gmail.com>
2021-08-06 02:47:04 +0800

    Tweak scripts
    - Prevent leak of user file structure in zip
    - Ensure system.json gets bundled inside system.zip so no PATH error pops up

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -2,7 +2,12 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 rm -rf $script_dir/../dist &&
   mkdir $script_dir/../dist &&
   # Ensure system.json is inside dist for release
+  cp $script_dir/../src/system.json $script_dir/../build/system.json &&
   cp $script_dir/../src/system.json $script_dir/../dist/system.json &&
   # Ensure template.json is inside the system.zip source
   cp $script_dir/../src/template.json $script_dir/../build/template.json &&
-  zip -vr $script_dir/../dist/system.zip $script_dir/../build -x ".DS_Store"
+  # Ensure non TS and SCSS files inside the build
+  cp -vr $script_dir/../src/lang $script_dir/../build/lang &&
+  cp -vr $script_dir/../src/templates $script_dir/../build/templates &&
+  cd $script_dir/../build &&
+  zip -vr ../dist/system.zip . -x ".DS_Store"
