981796e1aa6f43670264eff41a1c2788aa58999e
callunaborealis <callunaborealis@gmail.com>
2022-01-22 22:21:44 +0800

    Restore default build script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 
 script_dir="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 && pwd -P)"
-
-manifest_url="https://github.com/callunaborealis/fvtt-mutants-and-masterminds-3e/releases/latest/download/system.json"
-gh release create v0.1.0 $script_dir/../dist/system.json $script_dir/../dist/system.zip \
+version="v0.0.0"
+manifest_url="https://github.com/callunaborealis/fvtt-mutants-and-masterminds-3e/releases/$version/download/system.json"
+gh release create $version $script_dir/../dist/system.json $script_dir/../dist/system.zip \
   --notes "Link to manifest URL: $manifest_url" &&
   echo $manifest_url
--- scripts/replace.sh
+++ scripts/replace.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 script_dir="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 && pwd -P)"
-
-manifest_url="https://github.com/callunaborealis/fvtt-mutants-and-masterminds-3e/releases/latest/download/system.json"
-gh release upload v0.1.0 $script_dir/../dist/system.json $script_dir/../dist/system.zip --clobber
+version="v0.1.0"
+manifest_url="https://github.com/callunaborealis/fvtt-mutants-and-masterminds-3e/releases/$version/download/system.json"
+gh release upload $version $script_dir/../dist/system.json $script_dir/../dist/system.zip --clobber
 echo $manifest_url
