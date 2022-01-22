787aa5af2e267975100ac837fde8fb603191e7b3
callunaborealis <callunaborealis@gmail.com>
2022-01-22 22:39:58 +0800

    Fix broken link
    Should be releases/download/** not releases/**/download
    
    Related to https://github.com/callunaborealis/fvtt-mutants-and-masterminds-3e/issues/3

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,7 +2,7 @@
 
 script_dir="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 && pwd -P)"
 version="v0.0.0"
-manifest_url="https://github.com/callunaborealis/fvtt-mutants-and-masterminds-3e/releases/$version/download/system.json"
+manifest_url="https://github.com/callunaborealis/fvtt-mutants-and-masterminds-3e/releases/download/$version/system.json"
 gh release create $version $script_dir/../dist/system.json $script_dir/../dist/system.zip \
   --notes "Link to manifest URL: $manifest_url" &&
   echo $manifest_url
--- scripts/replace.sh
+++ scripts/replace.sh
@@ -2,6 +2,6 @@
 
 script_dir="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 && pwd -P)"
 version="v0.1.0"
-manifest_url="https://github.com/callunaborealis/fvtt-mutants-and-masterminds-3e/releases/$version/download/system.json"
+manifest_url="https://github.com/callunaborealis/fvtt-mutants-and-masterminds-3e/releases/download/$version/system.json"
 gh release upload $version $script_dir/../dist/system.json $script_dir/../dist/system.zip --clobber
 echo $manifest_url
