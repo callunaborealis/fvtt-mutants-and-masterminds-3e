8069b674dbd4286feb4893125dc1622999a6878f
callunaborealis <callunaborealis@gmail.com>
2022-01-22 22:04:43 +0800

    Remove echo from script dir

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-script_dir="echo $(cd -- "$(dirname "$0")" >/dev/null 2>&1 && pwd -P)"
+script_dir="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 && pwd -P)"
 
 manifest_url="https://github.com/callunaborealis/fvtt-mutants-and-masterminds-3e/releases/latest/download/system.json"
 gh release create v0.1.0 $script_dir/../dist/system.json $script_dir/../dist/system.zip \
